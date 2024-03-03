af75b7f62d5e3f4e461d6cdf3f4b77c578e33516
Vinayak Sharma <vinayak.sharma7149@gmail.com>
2024-03-04 00:43:19 +0530

    login animation

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React,{useState,useEffect} from 'react';
+import { AccountBox } from './components/accountBox'
 function App() {
   const [data,setData] = useState([]);
   useEffect(()=>{
@@ -13,18 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <AccountBox />
       </header>
     </div>
   );
---
+++ sample/src/components/accountBox/index.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import styled from "styled-components"
+
+const BoxContainer = styled.div`
+    width: 280px;
+    min-height: 550px;
+    display: flex;
+    flex-direction; column;
+    border-radius: 19px;
+    background-color: #fff;
+    box-shadow: 0 0 2px rgba(15,15,15,0.28);
+    position: relative;
+    overflow: hidden;
+`;
+
+const TopContainer = styled.div`
+    width: 100%
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    padding: 0 1.8em;
+    padding-bottom: 5em;
+`;
+
+const BackDrop = styled.div`
+    width: 160%;
+    height: 550px;    
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    border-radius: 50%;
+    top: -290px;
+    left: -70px;
+    transform: rotate(60deg);
+    background: rgb(241,196,15);
+    background: linear-gradient(90deg, rgba(241,196,15,1) 20%, rgba(243,172,18,1) 100%);
+
+`;
+
+const HeaderContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+const HeaderText = styled.h2`
+    font-size: 30px;
+    font-weight: 600;
+    line-height: 1.24;
+    color: #fff;
+    z-index: 10;
+    margin: 0;
+`;
+
+const SmallText = styled.h5`
+    color: #fff;
+    font-weight: 500;
+    font-size: 11px;
+    z-index: 10;
+    margin: 0;
+    margin-top: 7px;
+`;
+
+const InnerContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 0 1.8em;
+`;
+
+const backdropVariants = {
+    expanded: {
+        width: "233%",
+        height: "1050px",
+        borderRadius: "20%",
+        transform: "rotate(60deg)",
+    },
+    collapsed: {
+        width: "160%",
+        height: "550px",
+        borderRadius: "50%",
+        transform: "rotate(60deg)",
+    },
+};
+
+const expandingTransition = {
+    type: "spring",
+    duration: 2.3,
+    stiffness: 30,
+};
+
+
+
+export function AccountBox(props) {
+    return
+    <BoxContainer>
+        <TopContainer>
+            <BackDrop />
+            <HeaderContainer>
+                <HeaderText>Welcome</HeaderText>
+                <HeaderText>back</HeaderText>
+            </HeaderContainer>
+        </TopContainer>
+    </BoxContainer>
+}
