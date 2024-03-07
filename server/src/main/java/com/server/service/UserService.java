c3b4374596b96d94a2df51d1eb4c8ea33120913c
Ajin A N <ajin.an@incedoinc.com>
2024-03-07 12:31:31 +0530

    User service updated

--- server/src/main/java/com/server/service/UserService.java
+++ server/src/main/java/com/server/service/UserService.java
@@ -1,37 +1,65 @@
 package com.server.service;
-
+ 
+import com.server.bean.Judge;
+import com.server.bean.Panelist;
+import com.server.bean.Participant;
 import com.server.bean.User;
+import com.server.repo.JudgeRepo;
+import com.server.repo.PanelistRepo;
+import com.server.repo.ParticipantRepo;
 import com.server.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+ 
 @Service
 public class UserService {
-
-    private final UserRepo userRepository;
-
+ 
+    private  UserRepo userRepository;
+    private ParticipantRepo participantRepo;
+    private JudgeRepo judgeRepo;
+    private PanelistRepo panelistRepo;
+ 
     @Autowired
-    public UserService(UserRepo userRepository) {
+    public UserService(UserRepo userRepository,ParticipantRepo participantRepo, JudgeRepo judgeRepo,PanelistRepo panelistRepo) {
         this.userRepository = userRepository;
+        this.participantRepo=participantRepo;
+        this.judgeRepo=judgeRepo;
+        this.panelistRepo=panelistRepo;
     }
-
+ 
     public User registerUser(User user) {
-        // Set a default role (you can customize this based on your requirements)
         if (user.getRole() == null) {
             user.setRole("PARTICIPANT");
         }
-
-        // Suggest a username without special characters
-//        String suggestedUsername = suggestUsername(user.getUsername());
-//        user.setUsername(suggestedUsername);
-//
-//        // Suggest a password with at least some symbols
-//        String suggestedPassword = suggestPassword(user.getPassword());
-//        user.setPassword(suggestedPassword);
-
-        return userRepository.save(user);
+ 
+        User savedUser = userRepository.save(user);
+ 
+        // Create instances of Judge and Panelist before the switch statement
+        Judge judge = null;
+        Panelist panelist = null;
+ 
+        // Based on the user's role, save to the corresponding repository
+        switch (user.getRole()) {
+            case "PARTICIPANT":
+                Participant participant = new Participant(user.getUsername(), user.getEmail(), null);
+                participantRepo.save(participant);
+                break;
+            case "JUDGE":
+                judge = new Judge(user.getUsername(), user.getEmail());
+                judgeRepo.save(judge);
+                break;
+            case "PANELIST":
+                panelist = new Panelist(user.getUsername(), user.getEmail());
+                panelistRepo.save(panelist);
+                break;
+        }
+ 
+        // Additional logic for handling the saved user, if needed
+ 
+        return savedUser;
     }
-
+ 
+ 
     public User getUserByUsername(String username) {
         return userRepository.findByUsername(username);
     }
@@ -39,5 +67,5 @@ public class UserService {
     {
     	userRepository.save(user);
     }
-
+ 
 }
