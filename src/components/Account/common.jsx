a9d8af914c16c8b2978ca80979ce1c8561313baa
Rahul Kalia <rahulkalia660@gmail.com>
2024-03-12 12:57:19 +0530

    updated login css

--- src/components/Account/common.jsx
+++ src/components/Account/common.jsx
@@ -2,108 +2,76 @@ import styled from "styled-components";
 
 export const BoxContainer = styled.div`
   width: 100%;
-
+  max-width: 320px;
   display: flex;
-
   flex-direction: column;
-
-  align-items: center;
-
-  margin-top: 10px;
+  border-radius: 20px;
+  background-color: rgba(10, 25, 47, 0.95); /* Increased opacity */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  padding: 20px;
+  position: relative;
+  overflow: hidden;
+  border: 2px solid rgba(200, 200, 200, 0.3); /* Improved border styling */
 `;
 
 export const FormContainer = styled.form`
   width: 100%;
-
   display: flex;
-
   flex-direction: column;
-
-  box-shadow: 0px 0px 2.5px rgba(15, 15, 15, 0.19);
 `;
 
 export const MutedLink = styled.a`
   font-size: 11px;
-
   color: rgba(200, 200, 200, 0.8);
-
   font-weight: 500;
-
   text-decoration: none;
 `;
 
 export const BoldLink = styled.a`
   font-size: 11px;
-
   color: rgb(241, 196, 15);
-
   font-weight: 500;
-
   text-decoration: none;
-
   margin: 0 4px;
 `;
 
 export const Input = styled.input`
   width: 100%;
-
   height: 42px;
-
   outline: none;
-
-  border: 1px solid rgba(200, 200, 200, 0.3);
-
-  padding: 0px 10px;
-
+  border: none;
   border-bottom: 1.4px solid transparent;
-
   transition: all 200ms ease-in-out;
-
   font-size: 12px;
-
+  color: #fff;
+  background-color: transparent;
   &::placeholder {
     color: rgba(200, 200, 200, 1);
   }
-
   &:not(:last-of-type) {
     border-bottom: 1.5px solid rgba(200, 200, 200, 0.4);
   }
-
   &:focus {
-    outline: none;
-
     border-bottom: 2px solid rgb(241, 196, 15);
   }
 `;
 
 export const SubmitButton = styled.button`
   width: 100%;
-
   padding: 11px 40%;
-
   color: #fff;
-
   font-size: 15px;
-
   font-weight: 600;
-
   border: none;
-
-  border-radius: 100px 100px 100px 100px;
-
+  border-radius: 100px;
   cursor: pointer;
-
   transition: all, 240ms ease-in-out;
-
   background: rgb(241, 196, 15);
-
   background: linear-gradient(
     58deg,
     rgba(241, 196, 15, 1) 20%,
-
     rgba(243, 172, 18, 1) 100%
   );
-
   &:hover {
     filter: brightness(1.03);
   }
