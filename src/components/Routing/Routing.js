1d107576e81b1c0f8098776a77b9627b4be41619
VarishSanghvi <varishsanghvi155@gmail.com>
2024-03-13 15:10:47 +0530

    hu

--- src/components/Routing/Routing.js
+++ src/components/Routing/Routing.js
@@ -43,8 +43,6 @@ function Routing() {
           <Route path='/' element={<Home/>}/>
           <Route path="/home" element={<Home />} />
           <Route path="/dashboard" element={<ParticipantDashboard />} />
-          <Route path="/judge" element={<JudgeDashboard/>}/>
-          <Route path="/judge2" element={<JudgeDashboard2/>}/>
           <Route path='/panelist' element={<PanelistDashboard/>}/>
           <Route path="/admin" element={<AdminDashboard/>}/>
           <Route path="/judge" element={<JudgeDashboard/>}/>
