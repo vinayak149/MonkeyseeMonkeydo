f926f52f2c0c37ca24a82a12cd2367bbee055844
Vinayak Sharma <vinayak.sharma7149@gmail.com>
2024-03-15 03:08:31 +0530

    signup and registration integration

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -2,20 +2,52 @@ import { useFetchWrapper } from "../utils/fetchWrapper";
 
 export const AuthService = () => {
   const fetchWrapper = useFetchWrapper();
-  const baseURL = "auth";
+  const baseURL = "/auth"; // Updated base URL
 
   function login(email, password) {
     let url = `${baseURL}/login`;
     return fetchWrapper.post(url, {
-      username: email,
+      email: email, // Updated to match the backend endpoint
       password: password,
     });
   }
 
-  function register(data) {
+  function register(user) {
     let url = `${baseURL}/register`;
-    return fetchWrapper.post(url, data);
+    return fetchWrapper.post(url, user);
   }
 
-  return { login };
+  function verifyOtp(email, otp) {
+    let url = `${baseURL}/verify-otp`;
+    return fetchWrapper.post(url, {
+      email: email,
+      otp: otp,
+    });
+  }
+
+  function sendOtp(username) {
+    let url = `${baseURL}/send-otp`;
+    return fetchWrapper.post(url, {
+      username: username,
+    });
+  }
+
+  function forgotPassword(email) {
+    let url = `${baseURL}/forgot-password`;
+    return fetchWrapper.post(url, {
+      email: email,
+    });
+  }
+
+  function resetPassword(email, otp, password, confirmPassword) {
+    let url = `${baseURL}/reset-password`;
+    return fetchWrapper.post(url, {
+      email: email,
+      otp: otp,
+      password: password,
+      confirmPassword: confirmPassword,
+    });
+  }
+
+  return { login, register, verifyOtp, sendOtp, forgotPassword, resetPassword };
 };
---
+++ src/service/idea.service.js
@@ -0,0 +1,33 @@
+import { useFetchWrapper } from "../utils/fetchWrapper";
+
+export const IdeaService = () => {
+  const fetchWrapper = useFetchWrapper();
+  const baseURL = "ideas";
+
+  const getAllIdeas = () => {
+    const url = `${baseURL}/all`;
+    return fetchWrapper.get(url);
+  };
+
+  const getIdeaById = (id) => {
+    const url = `${baseURL}/${id}`;
+    return fetchWrapper.get(url);
+  };
+
+  const addIdea = (idea) => {
+    const url = `${baseURL}/add`;
+    return fetchWrapper.post(url, idea);
+  };
+
+  const updateIdea = (id, idea) => {
+    const url = `${baseURL}/update/${id}`;
+    return fetchWrapper.put(url, idea);
+  };
+
+  const deleteIdea = (id) => {
+    const url = `${baseURL}/delete/${id}`;
+    return fetchWrapper.delete(url);
+  };
+
+  return { getAllIdeas, getIdeaById, addIdea, updateIdea, deleteIdea };
+};
