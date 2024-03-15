29b669b3473cbcefb2056bc2463286682b2aa175
Vinayak Sharma <vinayak.sharma7149@gmail.com>
2024-03-16 03:43:57 +0530

    services and dashboard integration

--- client/src/service/judge.service.js
+++ client/src/service/judge.service.js
@@ -3,20 +3,37 @@ import { useFetchWrapper } from "../utils/fetchWrapper";
 export const JudgeService = () => {
   const fetchWrapper = useFetchWrapper();
   const baseURL = "judge";
-  function getJudges() {
+
+  function getAllJudges() {
     let url = `${baseURL}/all`;
     return fetchWrapper.get(url);
   }
 
-  function getJudgeById(id) {
-    let url = `${baseURL}/${id}`;
-    return fetchWrapper.get(url);
+  function addJudge(judgeData) {
+    let url = `${baseURL}/add`;
+    return fetchWrapper.post(url, judgeData);
   }
 
-  // function addJudges()
+  function updateJudge(judgeId, updatedJudge) {
+    let url = `${baseURL}/update/${judgeId}`;
+    return fetchWrapper.put(url, updatedJudge);
+  }
+
+  function deleteJudge(judgeId) {
+    let url = `${baseURL}/delete/${judgeId}`;
+    return fetchWrapper.delete(url);
+  }
+
+  function updateRatingAndFeedback(judgeId, ideaId, requestBody) {
+    let url = `${baseURL}/${judgeId}/giverating/${ideaId}`;
+    return fetchWrapper.put(url, requestBody);
+  }
 
   return {
-    getJudges,
-    getJudgeById,
+    getAllJudges,
+    addJudge,
+    updateJudge,
+    deleteJudge,
+    updateRatingAndFeedback,
   };
 };
